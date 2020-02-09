d57539b74de9aedd2d2434d79d011782391f2f85
ronlim415 <60123575+ronlim415@users.noreply.github.com>
2020-02-09 22:27:49 +0800

    Create class.movie.php

---
+++ class.movie.php
@@ -0,0 +1,24 @@
+<?php
+class movie{
+	public $database;
+	
+	public function __construct(){
+		$this->database = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+		if(mysqli_connect_errno()){
+			echo "Database connection error.";
+			exit;
+		}else {
+		}
+	}
+	
+	public function get_browser(){
+		$sql = "SELECT * FROM tbltitle";
+		$result = mysqli_query($this->database,$sql);
+		while($row = mysqli_fetch_assoc($result)){
+			$list[] = $row;
+		}
+		return $list;
+	}
+	
+}
+?>
