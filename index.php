3a0c09fe789a6e2882af784ee33ad167ea50355b
ronlim415 <60123575+ronlim415@users.noreply.github.com>
2020-02-10 09:07:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$json = file_get_contents("https://exercise2json.herokuapp.com/json.php");
+$json = file_get_contents("https://exe2.herokuapp.com/json.php");
 
 $data = json_decode($json,true);
 $list = $data['movie'];
